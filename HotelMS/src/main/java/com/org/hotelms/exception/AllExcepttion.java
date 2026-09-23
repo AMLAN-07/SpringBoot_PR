78bcb6eb643c075ab748cca67080bf9a598f3992
ABHINASH SAHOO <abinashsahoo626@gmail.com>
2026-09-23 13:44:12 +0530

    updatd

--- HotelMS/src/main/java/com/org/hotelms/exception/AllExcepttion.java
+++ HotelMS/src/main/java/com/org/hotelms/exception/AllExcepttion.java
@@ -1,5 +1,7 @@
 package com.org.hotelms.exception;
 
+import com.org.hotelms.payload.ApiResponse;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -7,7 +9,9 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class AllExcepttion {
     @ExceptionHandler(UserNotFoundException.class)
-    public ResponseEntity<> handelUserNotFoundException(){
-        return null;
+    public ResponseEntity<ApiResponse> handelUserNotFoundException(UserNotFoundException unfe) {
+        String message = unfe.getMessage();
+        ApiResponse response=ApiResponse.builder().message(message).status(HttpStatus.NOT_FOUND).build();
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
     }
 }
---
+++ HotelMS/src/main/java/com/org/hotelms/payload/ApiResponse.java
@@ -0,0 +1,14 @@
+package com.org.hotelms.payload;
+
+import lombok.*;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ApiResponse {
+    String message;
+    HttpStatus status;
+}
